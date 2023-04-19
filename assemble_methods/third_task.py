eb6b88ea987cee5c35f0823f91cfa530f7cfbe41
LysenkoAleksandra <orange260302@gmail.com>
2023-04-20 00:31:36 +0300

    Use SimpleInputer instead of getting rid of nan, use OrdinalEncoder

--- assemble_methods/third_task.py
+++ assemble_methods/third_task.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
 
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OrdinalEncoder
 
 from sklearn import preprocessing
 from sklearn import utils
@@ -15,51 +17,57 @@ from sklearn.linear_model import LogisticRegression
 
 from first_task import prepare_data
 
-def titanic_map(element):
-    if element == 'S':
-        return 1
-    elif element == 'Q':
-        return 0
-    elif element == 'C':
-        return -1
-    elif element == 'male':
-        return 5
-    elif element == 'female':
-        return -5
-    else:
-        return element
-
 def third_task():
     df_train = pd.read_csv(r'files\titanic_train.csv', sep=',')
     df_test = pd.read_csv(r'files\titanic_test.csv', sep=',')
 
     df_train=df_train.drop('Survived', axis=1)
-
+    df_train = df_train.drop('PassengerId', axis=1)
+    df_test = df_test.drop('PassengerId', axis=1)
     df_train =df_train.drop('Name', axis=1)
     df_test = df_test.drop('Name', axis=1)
 
-    df_train = df_train.drop('Ticket', axis=1)
-    df_test = df_test.drop('Ticket', axis=1)
+    enc = OrdinalEncoder()
+    enc.fit(df_train)
+    df_train =pd.DataFrame(enc.transform(df_train))
+    enc.fit(df_test)
+    df_test = pd.DataFrame(enc.transform(df_test))
 
-    df_train = df_train.drop('Sex', axis=1)
-    df_test = df_test.drop('Sex', axis=1)
 
-    df_train = df_train.drop('Embarked', axis=1)
-    df_test = df_test.drop('Embarked', axis=1)
 
-    df_train = df_train.drop('Cabin', axis=1)
-    df_test = df_test.drop('Cabin', axis=1)
+    # df_train=df_train.drop('Survived', axis=1)
+    #
+    # df_train =df_train.drop('Name', axis=1)
+    # df_test = df_test.drop('Name', axis=1)
+    #
+    # df_train = df_train.drop('Ticket', axis=1)
+    # df_test = df_test.drop('Ticket', axis=1)
+    #
+    # df_train = df_train.drop('Sex', axis=1)
+    # df_test = df_test.drop('Sex', axis=1)
+    #
+    # df_train = df_train.drop('Embarked', axis=1)
+    # df_test = df_test.drop('Embarked', axis=1)
 
-    df_train.dropna(inplace=True)
-    indices_to_keep = ~ df_train.isin([np.nan, np.inf, -np.inf]).any(axis=1)
-    df_train=df_train[indices_to_keep].astype(np.float64)
+    # df_train = df_train.drop('Cabin', axis=1)
+    # df_test = df_test.drop('Cabin', axis=1)
 
-    df_test.dropna(inplace=True)
-    indices_to_keep = ~ df_test.isin([np.nan, np.inf, -np.inf]).any(axis=1)
-    df_test = df_test[indices_to_keep].astype(np.float64)
+    imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+    imp.fit(df_train)
+    df_train=pd.DataFrame(imp.transform(df_train))
+    df_test = pd.DataFrame(imp.transform(df_test))
 
-    train_data, train_target = prepare_data(df_train, titanic_map, False)
-    test_data, test_target = prepare_data(df_test, titanic_map, False)
+
+    # df_train.dropna(inplace=True)
+    # indices_to_keep = ~ df_train.isin([np.nan, np.inf, -np.inf]).any(axis=1)
+    # df_train=df_train[indices_to_keep].astype(np.float64)
+
+    # df_test.dropna(inplace=True)
+    # indices_to_keep = ~ df_test.isin([np.nan, np.inf, -np.inf]).any(axis=1)
+    # df_test = df_test[indices_to_keep].astype(np.float64)
+
+    train_data, train_target = prepare_data(df_train, None, False)
+    test_data, test_target = prepare_data(df_test, None, False)
 
     lab = preprocessing.LabelEncoder()
     train_target = lab.fit_transform(train_target)
